ed09a26288cfab65a6df9aa71141ff05e1225bde
eliseugssantos <eliseu.gssantos@senacsp.edu.br>
2025-03-28 15:54:48 -0300

    2803

--- Folha de Pagamen/frmFolhaDePagamento.cs
+++ Folha de Pagamen/frmFolhaDePagamento.cs
@@ -15,6 +15,44 @@ namespace Folha_de_Pagamen
         public frmFolhaDePagamento()
         {
             InitializeComponent();
+            desbilitar();
+        }
+
+        private void desbilitar()
+        {
+            txtImpostoDeRenda.Enabled = false;
+            txtSalarioFolha.Enabled = false;
+            txtSalárioLiquido.Enabled = false;
+        }
+        private void limparCampos() 
+        {
+            txtImpostoDeRenda.Clear();
+            txtSalarioFolha.Clear();
+            txtSalárioLiquido.Clear();
+            txtSalario.Clear();
+            ckbPlanoDeSaude.Checked = false;
+            cbbClubeDeazer.Enabled = false;
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            limparCampos();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double salario,salarioFolha, salarioliquido = 0, impostoderenda;
+
+            salarioFolha = Convert.ToDouble(txtSalarioFolha);
+            salario = Convert.ToDouble(txtSalario);
+            salarioFolha = salario;
+
+             
         }
     }
 }
